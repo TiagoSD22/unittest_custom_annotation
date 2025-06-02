a4cdbaf2dd64f3f567eabed575755d3bbae217fa
Tiago Dionizio <tiagosdionizio@gmail.com>
2025-06-02 13:04:37 -0300

    added a test case to validate parametrize with multiple variables

--- unittestpoc/app.py
+++ unittestpoc/app.py
@@ -7,8 +7,13 @@ from unittestpoc.annotations.parametrize import parametrize
 class MyTestCase(unittest.TestCase):
     @parametrize([1, 2, 3])
     def test_example(self, value):
-        print(f"Running test with value: {value}")
+        print(f"Running parametrized test with value: {value}")
         self.assertTrue(value > 0)
+
+    @parametrize("x, y", [(1, 2), (3, 4)])
+    def test_example_multi(self, x, y):
+        print(f"Running parametrized multiple variables test with x={x}, y={y}")
+        self.assertTrue(x < y)
 
 if __name__ == "__main__":
     unittest.main()
