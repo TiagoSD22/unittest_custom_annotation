98ac6b556e0f64538c880cd188cc1d4a6b50c472
Tiago Dionizio <tiagosdionizio@gmail.com>
2025-06-02 13:04:21 -0300

    added support to handle multiple variables in each iteration of the test method

--- unittestpoc/annotations/parametrize.py
+++ unittestpoc/annotations/parametrize.py
@@ -1,11 +1,22 @@
 import unittest
 from functools import wraps
 
-def parametrize(values):
+def parametrize(variables_or_values, values=None):
     """
-    Custom annotation to parametrize a test method with different values
-    since unittest package does not have a parametrize option.
+    Custom annotation to parametrize a test method with optional variables
+    and a list of values or tuples of values for each execution.
     """
+    if values is None:
+        # If only a list of values is provided, treat it as single-variable cases
+        variables = ["value"]
+        values = variables_or_values
+    else:
+        # Handle the case where variables and values are provided
+        if isinstance(variables_or_values, str):
+            variables = [var.strip() for var in variables_or_values.split(",")]
+        else:
+            raise TypeError("Variables must be a string of comma-separated names.")
+
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
@@ -14,8 +25,12 @@ def parametrize(values):
             if not isinstance(instance, unittest.TestCase):
                 raise TypeError("The @parametrize annotation can only be applied to methods in a class that extends unittest.TestCase.")
 
-            # Execute the test method for each value in the list
-            for value in values:
-                func(*args, value, **kwargs)
+            # Execute the test method for each tuple of values
+            for value_tuple in values:
+                if not isinstance(value_tuple, tuple):
+                    value_tuple = (value_tuple,)
+                if len(value_tuple) != len(variables):
+                    raise ValueError("Each tuple of values must match the number of variables.")
+                func(*args, **dict(zip(variables, value_tuple)), **kwargs)
         return wrapper
     return decorator
