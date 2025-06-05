85571e17101cf14030eca3796d6c172c3dcf639e
Tiago Siqueira Dionizio <tiagosdionizio@gmail.com>
2025-06-04 23:26:26 -0300

    added test case using threads for parametrize

--- unittestpoc/app.py
+++ unittestpoc/app.py
@@ -20,5 +20,11 @@ class MyTestCase(unittest.TestCase):
         print(f"Running parametrized multiple variables without label test with x={x}, y={y}")
         self.assertTrue(x < y)
 
+    # Parallel execution with 3 threads
+    @parametrize([(1, 2), (3, 4), (5, 6), (7, 8), (9, 10)], threads=3)
+    def test_example_multi_threaded(self, x, y):
+        print(f"Running threaded test with x={x}, y={y} on thread {threading.current_thread().name}")
+        self.assertTrue(x < y)
+
 if __name__ == "__main__":
     unittest.main()
